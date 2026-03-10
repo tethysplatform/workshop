4ba9ea0005c350627087a1fc7cf0f6b9c6108e27
Nathan <nswain@aquaveo.com>
2026-03-10 10:25:49 -0600

    Setup to deploy on GH pages.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://tethysplatform.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/workshop/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'tethysplatform', // Usually your GitHub org/user name.
+  projectName: 'workshop', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
