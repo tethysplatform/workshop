4eab97ea7a6c29dbc7be763df6e2919449adcb04
Nathan <nswain@aquaveo.com>
2026-05-15 16:32:29 -0600

    Remove unused SVG file: undraw_docusaurus_tree.svg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Tethys Platform Workshop',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
